238dc57559ba7ab80cb91718e622926155db44a9
João Ragazzo <joao_ragazzo@hotmail.com>
2023-12-23 04:08:08 -0300

    refact: implementing strategy pattern to parserservice

--- log4z/src/main/java/com/log4z/services/ParserService.java
+++ log4z/src/main/java/com/log4z/services/ParserService.java
@@ -8,6 +8,10 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 @Service
 @Getter
@@ -16,27 +20,19 @@ import org.springframework.stereotype.Service;
 @NoArgsConstructor
 public class ParserService {
 
-    private C4Parser c4Parser = new C4Parser();
-    private HacksawParser hacksawParser = new HacksawParser();
-    private AttachLockParser attachLockParser = new AttachLockParser();
-    private AccessLockParser accessLockParser = new AccessLockParser();
+    private List<Parser> parsers = Arrays.asList(
+            new C4Parser(),
+            new HacksawParser(),
+            new AttachLockParser(),
+            new AccessLockParser()
+    );
 
     public Parser IdentifyRegex(String log) throws LogFormatNotRecognized {
 
-        if (c4Parser.check(log)) {
-            return c4Parser;
-        }
-
-        if (hacksawParser.check(log)) {
-            return hacksawParser;
-        }
-
-        if (attachLockParser.check(log)) {
-            return attachLockParser;
-        }
-
-        if (accessLockParser.check(log)) {
-            return accessLockParser;
+        for(Parser parser : parsers) {
+            if (parser.check(log)) {
+                return parser;
+            }
         }
 
         throw new LogFormatNotRecognized("This log format was not recognized.");
