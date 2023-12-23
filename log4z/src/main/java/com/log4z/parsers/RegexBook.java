994a574d8571dd08e32893820e2fd7970b606f89
João Ragazzo <joao_ragazzo@hotmail.com>
2023-12-23 00:44:21 -0300

    refact: changing the default date header
    was possible to notice that this header is very commun among most logs, so we can reuse this

--- log4z/src/main/java/com/log4z/parsers/RegexBook.java
+++ log4z/src/main/java/com/log4z/parsers/RegexBook.java
@@ -2,6 +2,12 @@ package com.log4z.parsers;
 
 public class RegexBook {
 
+    /*
+        Default header for logs
+    */
+
+    public static final String defaultDateHeader = "\\[(\\d{2})-(\\d{2})-(\\d{4})\\].*";
+
 
     /*
         C4 Parsers
@@ -14,8 +20,6 @@ public class RegexBook {
     */
 
     public static final String HacksawHeader = "\\[\\d{2}-\\d{2}-\\d{4}]\\[\\d{2}\\.\\d{2}\\.\\d{2}] New Raid Log File Created!";
-
-    public static final String HacksawDate = "\\[(\\d{2})-(\\d{2})-(\\d{4})\\].*";
     public static final String HacksawBody = "\\[(\\d{2}):(\\d{2}):(\\d{2})\\] Player \\((.+)\\, (\\d+)\\)\\(<(\\d+\\.\\d+), (\\d+\\.\\d+), (\\d+\\.\\d+)>\\) is raiding lock! CodeLock HP: (\\d+) Damage Done: (\\d+)";
     public static final String HacksawBodySuccess = "\\[(\\d{2}):(\\d{2}):(\\d{2})\\] Player \\((.+)\\, (\\d+)\\)\\(<(\\d+\\.\\d+), (\\d+\\.\\d+), (\\d+\\.\\d+)>\\) successfully raided lock!";
 }
