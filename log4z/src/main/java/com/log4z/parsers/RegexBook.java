8a389f38901668bb925cd842d54aa552edfb5517
João Ragazzo <joao_ragazzo@hotmail.com>
2023-12-23 00:48:39 -0300

    feat: adding attack lock regex to regex book

--- log4z/src/main/java/com/log4z/parsers/RegexBook.java
+++ log4z/src/main/java/com/log4z/parsers/RegexBook.java
@@ -22,4 +22,13 @@ public class RegexBook {
     public static final String HacksawHeader = "\\[\\d{2}-\\d{2}-\\d{4}]\\[\\d{2}\\.\\d{2}\\.\\d{2}] New Raid Log File Created!";
     public static final String HacksawBody = "\\[(\\d{2}):(\\d{2}):(\\d{2})\\] Player \\((.+)\\, (\\d+)\\)\\(<(\\d+\\.\\d+), (\\d+\\.\\d+), (\\d+\\.\\d+)>\\) is raiding lock! CodeLock HP: (\\d+) Damage Done: (\\d+)";
     public static final String HacksawBodySuccess = "\\[(\\d{2}):(\\d{2}):(\\d{2})\\] Player \\((.+)\\, (\\d+)\\)\\(<(\\d+\\.\\d+), (\\d+\\.\\d+), (\\d+\\.\\d+)>\\) successfully raided lock!";
+
+
+    /*
+        Attach lock
+    */
+
+    public static final String LockHeader = "\\[\\d{2}-\\d{2}-\\d{4}]\\[\\d{2}\\.\\d{2}\\.\\d{2}] New Attach Log File Created!";
+    public static final String LockBody = "\\[(\\d{2}):(\\d{2}):(\\d{2})\\] Player \\((.+)\\, (\\d+)\\)\\(<(\\d+\\.\\d+), (\\d+\\.\\d+), (\\d+\\.\\d+)>\\) attached a lock!";
+
 }
